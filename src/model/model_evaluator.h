5c77699f9979fafa51b0f79c7a5b41dbc6c0c577
Arie Gurfinkel <arie.gurfinkel@uwaterloo.ca>
2017-06-20 23:54:32 -0400

    ported model_evaluator from z3/master

--- src/model/model_evaluator.h
+++ src/model/model_evaluator.h
@@ -35,11 +35,14 @@ public:
 
     ast_manager & m () const;
     void set_model_completion(bool f);
+    void set_expand_array_equalities(bool f);
 
     void updt_params(params_ref const & p);
     static void get_param_descrs(param_descrs & r);
 
     void operator()(expr * t, expr_ref & r);
+
+    expr_ref operator()(expr* t);
 
     void cleanup(params_ref const & p = params_ref());
     void reset(params_ref const & p = params_ref());
