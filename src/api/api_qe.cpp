c76e0a094c2cc9ecad3320314b8b7b73e1ffae14
Arie Gurfinkel <arie.gurfinkel@uwaterloo.ca>
2017-06-22 21:21:49 -0400

    (api) adjusted spacer api

--- src/api/api_qe.cpp
+++ src/api/api_qe.cpp
@@ -13,11 +13,11 @@
 
 #include "expr_map.h"
 
-extern "C" 
+extern "C"
 {
-  Z3_ast Z3_API Z3_qe_model_project (Z3_context c, 
-                                     Z3_model m, 
-                                     unsigned num_bounds, 
+  Z3_ast Z3_API Z3_qe_model_project (Z3_context c,
+                                     Z3_model m,
+                                     unsigned num_bounds,
                                      Z3_app const bound[],
                                      Z3_ast body)
   {
@@ -26,7 +26,7 @@ extern "C"
     RESET_ERROR_CODE();
 
     app_ref_vector vars(mk_c(c)->m ());
-    for (unsigned i = 0; i < num_bounds; ++i) 
+    for (unsigned i = 0; i < num_bounds; ++i)
     {
       app *a = to_app (bound [i]);
       if (a->get_kind () != AST_APP)
@@ -36,17 +36,17 @@ extern "C"
       }
       vars.push_back (a);
     }
-    
+
     expr_ref result (mk_c(c)->m ());
     result = to_expr (body);
     model_ref model (to_model_ref (m));
     spacer::qe_project (mk_c(c)->m (), vars, result, model);
     mk_c(c)->save_ast_trail (result.get ());
-    
+
     return of_expr (result.get ());
     Z3_CATCH_RETURN(0);
   }
-  
+
   Z3_ast Z3_API Z3_qe_model_project_skolem (Z3_context c,
                                        Z3_model m,
                                        unsigned num_bounds,
@@ -92,28 +92,28 @@ extern "C"
     }
 
   Z3_ast Z3_API Z3_model_extrapolate (Z3_context c,
-                                      Z3_model m, 
+                                      Z3_model m,
                                       Z3_ast fml)
   {
     Z3_TRY;
     LOG_Z3_model_extrapolate (c, m, fml);
     RESET_ERROR_CODE();
-    
+
     model_ref model (to_model_ref (m));
     expr_ref_vector facts (mk_c(c)->m ());
     facts.push_back (to_expr (fml));
     flatten_and (facts);
-    
+
     spacer::model_evaluator_util mev (mk_c(c)->m());
     mev.set_model (*model);
-    
+
     expr_ref_vector lits (mk_c(c)->m());
     spacer::compute_implicant_literals (mev, facts, lits);
-    
+
     expr_ref result (mk_c(c)->m ());
     result = mk_and (lits);
     mk_c(c)->save_ast_trail (result.get ());
-    
+
     return of_expr (result.get ());
     Z3_CATCH_RETURN(0);
   }
@@ -124,7 +124,7 @@ extern "C"
     LOG_Z3_qe_lite (c, vars, body);
     RESET_ERROR_CODE();
     ast_ref_vector &vVars = to_ast_vector_ref (vars);
-    
+
     app_ref_vector vApps (mk_c(c)->m());
     for (unsigned i = 0; i < vVars.size (); ++i)
     {
@@ -136,11 +136,12 @@ extern "C"
       }
       vApps.push_back (a);
     }
-        
+
     expr_ref result (mk_c(c)->m ());
     result = to_expr (body);
-    
-    qe_lite qe (mk_c(c)->m ());
+
+    params_ref p;
+    qe_lite qe (mk_c(c)->m (), p);
     qe (vApps, result);
 
     // -- copy back variables that were not eliminated
@@ -150,10 +151,10 @@ extern "C"
       for (unsigned i = 0; i < vApps.size (); ++i)
         vVars.push_back (vApps.get (i));
     }
-    
+
     mk_c(c)->save_ast_trail (result.get ());
     return of_expr (result);
     Z3_CATCH_RETURN(0);
   }
-  
+
 }
