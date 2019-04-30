c103a919a76150c460182d9a9bd16a68460043d2
Hari Govind V K <hgvk94@gmail.com>
2019-04-30 17:55:47 -0400

    rewrite method to identify patterns. Make depth of recursion clear. Avoid redundant work in earlier implementation

--- src/muz/spacer/spacer_abstraction.cpp
+++ src/muz/spacer/spacer_abstraction.cpp
@@ -39,71 +39,27 @@ namespace {
 
 namespace spacer {
 
-
-    namespace mono_var_ns{
-        struct found {
-            expr *evidence;
-            found(expr *e) : evidence(e) {}
-            expr * e() { return evidence; }
-        };
-        struct proc {
-            ast_manager m;
-            arith_util m_arith;
-            proc(ast_manager &man) : m(man), m_arith(m) {}
-            bool is_leq(expr *e) {
-                return get_num_vars(e) == 1 && !has_nonlinear_mul(e, m);
-            }
-            void operator()(var *n) {}
-            void operator()(quantifier *q) {}
-            void operator()(app *n){
-                if((m_arith.is_arith_expr(n) || m.is_eq(n)) && is_leq(n)){
-                    throw found(n);
-                }
-                if(m.is_and(n)){
-                    for(auto &c : *n){
-                        if(is_leq(c)) { throw found(c); }
-                    }
-                }
-            }
-        };
-    }
-
-    bool single_nonlinear_var_pattern(expr *pat, expr_ref &out){
-        mono_var_ns::proc proc(out.m());
-        try{
-            for_each_expr(proc, pat);
-        } catch (mono_var_ns::found &f){
-            out = f.e();
-            return true;
-        }
-        return false;
-    }
-
-
+bool is_leq(expr *pattern, ast_manager &m, arith_util &a_util)
+{
+  expr *e;
+  if (m.is_not(pattern, e)) return is_leq(e, m, a_util);
+  if (a_util.is_arith_expr(to_app(pattern)) || m.is_eq(pattern)) {
+    return get_num_vars(pattern) == 1 && !has_nonlinear_var_mul(pattern, m);
+  }
+  return false;
+}
 // a mono_var_pattern has only one variable in the whole expression and is
 // linear returns the literal with the variable
 bool context::mono_var_pattern(expr *pattern, expr_ref &leq_lit) {
     ast_manager &m = leq_lit.m();
     arith_util a_util(m);
-    if(is_uninterp_const(pattern))
-      return false;
-    // XXX does not handle equality
-    if (a_util.is_arith_expr(to_app(pattern)) || m.is_eq(pattern)) {
-        bool is_leq =
-            get_num_vars(pattern) == 1 && !has_nonlinear_mul(pattern, m);
-        if (is_leq) leq_lit = pattern;
-        return is_leq;
-    }
-    expr *e;
-    if (m.is_not(pattern, e)) { return mono_var_pattern(e, leq_lit); }
-    SASSERT(m.is_and(pattern));
-    // if the pattern has multiple literals, check whether one of them is leq
+    // if the pattern has multiple literals, check whether exact;y one of them is leq
     expr_ref_vector pattern_and(m);
     pattern_and.push_back(pattern);
     flatten_and(pattern_and);
     unsigned count = 0;
-    for (auto lit : pattern_and) {
-        if (mono_var_pattern(lit, leq_lit)) count++;
+    for (auto *lit : pattern_and) {
+      if (is_leq(lit, m, a_util)) { leq_lit = lit; count++; }
     }
     return count == 1;
 }
@@ -123,22 +79,14 @@ bool context::mono_coeff_lm(pob &n, expr_ref &lit) {
 
         expr *pattern = neighbours.get(0);
 
-        if (single_nonlinear_var_pattern(pattern, lit)) {
+        if (mono_var_pattern(pattern, lit)) {
+            // HG : store the pattern in the pob. Required because there could
+            // be multile patterns among lemmas
             TRACE("merge_dbg",
                   tout << "Found a pattern " << mk_pp(pattern, m) << "\n";);
             n.set_abs_pattern(pattern);
             return true;
         }
-
-        // if (mono_var_pattern(pattern, lit)) {
-        //     // HG : store the pattern in the pob. Required because there could
-        //     // be
-        //     // multile patterns among lemmas
-        //     TRACE("merge_dbg",
-        //           tout << "Found a pattern " << mk_pp(pattern, m) << "\n";);
-        //     n.set_abs_pattern(pattern);
-        //     return true;
-        // }
     }
     return false;
 }
