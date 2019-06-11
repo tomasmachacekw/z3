8e7af44dc1d6b1a5abb7033d5b9f9e860bdd29f5
Hari Govind V K <hgvk94@gmail.com>
2019-06-11 12:00:23 -0400

    introduce new action. Comment out old action

--- src/muz/spacer/spacer_merge_generalizer.cpp
+++ src/muz/spacer/spacer_merge_generalizer.cpp
@@ -257,9 +257,11 @@ bool lemma_merge_generalizer::half_plane_XX(
             conjectures.push_back(conj);
             conj = m_arith.mk_gt(t2, t1);
             conjectures.push_back(conj);
+            conj = m_arith.mk_le(m_arith.mk_sub(t2, t1),
+                                 m_arith.mk_numeral(k2 - k1, true));
             // dont forget we're in cube space!
             // so (2 * a < b) => (>= (* 2 a) b)
-            conj = m_arith.mk_ge(m_arith.mk_mul(m_arith.mk_int(2), t1), t2);
+            // conj = m_arith.mk_ge(m_arith.mk_mul(m_arith.mk_int(2), t1), t2);
             conjectures.push_back(conj);
             return true;
         }
