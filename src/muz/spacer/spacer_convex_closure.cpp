29eefa5bc2d9b23e997f21ba7a3b8903e094125b
Arie Gurfinkel <arie.gurfinkel@uwaterloo.ca>
2019-06-20 21:31:14 +0300

    Comments on convex closure code

--- src/muz/spacer/spacer_convex_closure.cpp
+++ src/muz/spacer/spacer_convex_closure.cpp
@@ -1,30 +1,29 @@
-#include "spacer_convex_closure.h"
-namespace {
-//rational a < rational b
-struct lt_rational {
-  bool operator()(rational const& a, rational const& b) const { return a < b; }
-};
-} // anonymous namespace
+#include "muz/spacer/spacer_convex_closure.h"
 
 namespace spacer {
-bool convex_closure::compute_cls(vector<rational>& data, expr*& cnst, expr_ref& cls)
-{
-  //TODO handle duplicates in data
-  //TODO use solver in z3
-  lt_rational lt;
-  std::sort(data.begin(), data.end(), lt);
-  rational cd = data[1] - data[0];
-  for(unsigned i = 1; i < data.size() - 1; ++i)
-    {
-      if (cd != data[i + 1] - data[i])
-        return false;
+bool convex_closure::compute_cls(vector<rational> &data, expr *&cnst,
+                                 expr_ref &cls) {
+    // TODO handle duplicates in data
+    // TODO use solver in z3
+    std::sort(
+        data.begin(), data.end(),
+        [](rational const &x, rational const &y) -> bool { return x < y; });
+    rational step = data[1] - data[0];
+    for (unsigned i = 1; i < data.size() - 1; ++i) {
+        if (step != data[i + 1] - data[i]) return false;
     }
-  //progression is (forall i cd*i + offset) where offset is d%cd for any d
-  app* cd_expr = m_arith.mk_numeral(cd, true);
-  app* d_expr = m_arith.mk_numeral(data.get(0), true);
-  app* lhs = m_arith.mk_mod(cnst, cd_expr);
-  app* rhs = m_arith.mk_mod(d_expr, cd_expr);
-  cls = expr_ref(m.mk_eq(lhs, rhs), m);
-  return true;
+    // AG: don't understand the comment, improve
+    // progression is (forall i step*i + offset) where offset is ( d % step )
+    // for any d
+    expr_ref cd_expr(m_arith.mk_numeral(step, true), m);
+    expr_ref d_expr(m_arith.mk_numeral(data.get(0), true), m);
+    expr_ref lhs(m_arith.mk_mod(cnst, cd_expr), m);
+    // AG: rhs is a number. It should be reduced to a number not to a mod expression!
+    expr_ref rhs(m_arith.mk_mod(d_expr, cd_expr), m);
+    // AG: simplify the case when rhs is 1
+    // AG: the result is not a convex closure because bounds on cnst are not returned
+    // AG: expected output is: data[0] <= cnst <= data[N] && cnst mod step == (data[0] mod step)
+    cls = expr_ref(m.mk_eq(lhs, rhs), m);
+    return true;
 };
-} // spacer namespace
+} // namespace spacer
--- src/muz/spacer/spacer_convex_closure.h
+++ src/muz/spacer/spacer_convex_closure.h
@@ -1,17 +1,44 @@
 #include "ast/arith_decl_plugin.h"
-#include "ast/ast_util.h"
 #include "ast/ast.h"
+#include "ast/ast_util.h"
 #include "muz/spacer/spacer_util.h"
 
-namespace spacer{
-class convex_closure{
-  ast_manager& m;
-  arith_util m_arith;
- public:
- convex_closure(ast_manager& man) : m(man), m_arith(m) {}
-  // compute the convex closure in one dimension. data is a set of numbers, cnst
-  // is the dimension and conj is the convex closure of the form ( cnst mod k = c )
-  // returns false if a convex closure with just one literal cannot be computed.
- bool compute_cls(vector<rational>& data, expr*& cnst, expr_ref& conj);
+namespace spacer {
+class convex_closure {
+    ast_manager &m;
+    arith_util m_arith;
+
+  public:
+    convex_closure(ast_manager &man) : m(man), m_arith(m) {}
+
+    // XXX Replace with an incremental interface suggested below
+
+    // compute the convex closure in one dimension. data is a set of numbers,
+    // cnst is the dimension and conj is the convex closure of the form ( cnst
+    // mod k = c ) returns false if a convex closure with just one literal
+    // cannot be computed.
+    bool compute_cls(vector<rational> &data, expr *&cnst, expr_ref &conj);
+
+    /// Incremental interface
+
+
+    /// \brief Name a dimension
+    void set_dimension(unsigned i, expr *v);
+    /// \brief Return number of dimensions of each point
+    unsigned dims();
+
+    /// \brief add one-dimensional point
+    void push_back(rational x);
+    /// \brief add two-dimensional point
+    void push_back(rational x, rational y);
+    /// \brief add three-dimensional point
+    void push_back(rational x, rational y, rational z);
+    /// \brief add n-dimensional point
+    void push_back(vector<rational> &point);
+
+    /// \brief compute convex closure of current set of points
+    /// return true if the closure is exact
+    bool closure(expr_ref &res);
+
 };
-}
+} // namespace spacer
