3aada4f40d77abe506822912b88bd1bfe99f2acd
hgvk94 <hgvk94@gmail.com>
2019-11-24 17:18:23 -0500

    use sage for kernel computation

---
+++ src/muz/spacer/spacer_sage_interface.h
@@ -0,0 +1,48 @@
+#pragma once
+#include "spacer_arith_kernel.h"
+#include "util/util.h"
+#include <fstream>
+#include <signal.h>
+#include <sys/wait.h>
+#include <unistd.h>
+namespace spacer {
+
+/*
+  Interface to Sage. Supports initialization and writing to sage. To get
+  output from Sage, write to file and read from the file. Could not find
+  standard methods to convert file descriptors to streams. Easier to write to
+  file and use ifstream
+ */
+class Sage {
+    FILE *m_out;
+    FILE *m_in;
+    std::string tmp_name;
+    pid_t child_pid;
+
+  public:
+    Sage();
+    bool test();
+    ~Sage() {
+        kill(child_pid, SIGKILL);
+        int status;
+        waitpid(child_pid, &status, 0);
+    }
+    FILE *get_ostream() const { return m_out; }
+    FILE *get_istream() const { return m_in; }
+};
+
+/*
+  Compute kernel using Sage.
+ */
+class Sage_kernel : public arith_kernel {
+    Sage m_sage;
+    bool compute_arith_kernel() override;
+    std::string print_matrix() const;
+    std::string print_kernel() const;
+
+  public:
+    Sage_kernel(spacer_matrix &matrix) : arith_kernel(matrix), m_sage() {}
+    ~Sage_kernel() override {}
+};
+
+} // namespace spacer
