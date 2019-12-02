7a17c1992c517de5679a1f9e991bc4fb922e29fd
hgvk94 <hgvk94@gmail.com>
2019-12-02 17:54:03 -0500

    print stats at every depth

--- src/muz/spacer/spacer_context.cpp
+++ src/muz/spacer/spacer_context.cpp
@@ -3080,6 +3080,7 @@ lbool context::solve_core (unsigned from_lvl)
                    if (m_params.print_statistics ()) {
                        statistics st;
                        collect_statistics (st);
+                       st.display_smt2(verbose_stream());
                    };
             );
 
