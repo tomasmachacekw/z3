ae97d9a766602d0905d437af636f1ac57f1de899
hgvk94 <hgvk94@gmail.com>
2019-12-10 00:38:37 -0500

    bug fix. pop solver in case mbp fails

--- src/muz/spacer/spacer_merge_generalizer.cpp
+++ src/muz/spacer/spacer_merge_generalizer.cpp
@@ -395,6 +395,7 @@ bool lemma_merge_generalizer::core(lemma_ref &lemma) {
     TRACE("merge_dbg_verb", tout << "Pattern after mbp of computing cvx cls: "
                                  << cvx_pattern << "\n";);
     if (m_dim_frsh_cnsts.size() > 0) {
+        m_solver->pop(1);
         TRACE("merge_dbg", tout << "could not eliminate all vars\n";);
         return false;
     }
