62f80a463ee7400eac6b17591c08524c2428fe01
hgvk94 <hgvk94@gmail.com>
2019-12-18 17:28:00 -0500

    move mono var into abstraction

--- src/muz/spacer/spacer_abstraction.cpp
+++ src/muz/spacer/spacer_abstraction.cpp
@@ -50,6 +50,11 @@ bool context::mono_coeff_lm(pob &n, expr_ref &lit) {
             lc->dec_gas();
             return true;
         }
+bool is_mono_var(expr *pattern, ast_manager &m, arith_util &a_util) {
+    expr *e;
+    if (m.is_not(pattern, e)) return is_mono_var(e, m, a_util);
+    if (a_util.is_arith_expr(to_app(pattern)) || m.is_eq(pattern)) {
+        return get_num_vars(pattern) == 1 && !has_nonlinear_var_mul(pattern, m);
     }
     return false;
 }
@@ -75,6 +80,24 @@ void context::set_nvr_abs(const pob_ref &pob_abs) {
     pob_abs->pt().get_all_lemmas(all_lemmas, false);
     for (auto *l : all_lemmas) {
         if (lc->can_contain(l)) l->get_pob()->set_can_abs(false);
+bool mono_var_pattern(const expr_ref &pattern, expr_ref &leq_lit) {
+    if (get_num_vars(pattern) != 1) return false;
+    ast_manager &m = leq_lit.m();
+    arith_util a_util(m);
+    // if the pattern has multiple literals, check whether exactly one of them
+    // is leq
+    expr_ref_vector pattern_and(m);
+    pattern_and.push_back(pattern);
+    flatten_and(pattern_and);
+    unsigned count = 0;
+    for (auto *lit : pattern_and) {
+        if (is_mono_var(lit, m, a_util)) {
+            leq_lit = lit;
+            count++;
+        }
+    }
+    return count == 1;
+}
     }
 }
 
