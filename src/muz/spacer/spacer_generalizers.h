4a69afcdc6af0f1a0644ba7ca087179957eb150f
hgvk94 <hgvk94@gmail.com>
2020-03-11 20:49:49 -0400

    add global generalizer

--- src/muz/spacer/spacer_generalizers.h
+++ src/muz/spacer/spacer_generalizers.h
@@ -21,6 +21,7 @@ Revision History:
 
 #include "ast/arith_decl_plugin.h"
 #include "muz/spacer/spacer_context.h"
+#include "muz/spacer/spacer_convex_closure.h"
 
 namespace spacer {
 
@@ -151,6 +152,84 @@ class lemma_quantifier_generalizer : public lemma_generalizer {
                  expr_ref &bind);
 
     bool find_stride(expr_ref_vector &c, expr_ref &pattern, unsigned &stride);
+};
+
+class lemma_global_generalizer : public lemma_generalizer {
+    struct stats {
+        unsigned m_num_cls_ofg;
+        unsigned m_num_syn_cls;
+        unsigned m_num_mbp_failed;
+        unsigned m_num_non_lin;
+        unsigned m_num_no_ovr_approx;
+        unsigned m_num_cant_abs;
+
+        stopwatch watch;
+        stats() { reset(); }
+        void reset() {
+            watch.reset();
+            m_num_cls_ofg = 0;
+            m_num_non_lin = 0;
+            m_num_syn_cls = 0;
+            m_num_mbp_failed = 0;
+            m_num_no_ovr_approx = 0;
+            m_num_cant_abs = 0;
+        }
+    };
+
+    ast_manager &m;
+    arith_util m_arith;
+    stats m_st;
+    convex_closure m_cvx_cls;
+    // save fresh constants for mbp
+    app_ref_vector m_dim_frsh_cnsts;
+    expr_ref_vector m_dim_vars;
+    bool m_exact;
+    // solver to get model for computing mbp and to check whether cvx_cls ==>
+    // mbp
+    ref<solver> m_solver;
+    bool subsume(lemma_cluster lc, lemma_ref &l, expr_ref_vector &res);
+
+  public:
+    lemma_global_generalizer(context &ctx);
+    ~lemma_global_generalizer() override {}
+    void operator()(lemma_ref &lemma) override;
+    void collect_statistics(statistics &st) const override;
+    void reset_statistics() override { m_st.reset(); }
+
+  private:
+    // TODO: define state independent functions somewhere else
+    bool core(lemma_ref &lemma);
+
+    // create new vars to compute convex cls
+    void add_dim_vars(const lemma_cluster &lc);
+
+    // mark frsh constants as to_real
+    void rewrite_frsh_cnsts();
+
+    // collect substituted numerals from cluster and add to m_cvx_cls
+    void add_int_points(const lemma_cluster &lc);
+
+    // reset state
+    void reset(unsigned n_vars);
+
+    // replaces vars with uninterpreted constants in fml
+    void var_to_const(expr *fml, expr_ref &rw_fml);
+
+    // rewrite all uninterpreted constants in e to real
+    void to_real(const expr_ref_vector &e, expr_ref &rw_e);
+
+    // rewrite all uninterpreted constants in e to real
+    void to_real(expr_ref &fml);
+
+    // convert reals to ints by multiplying with lcm of denominators
+    void normalize(expr_ref &fml);
+
+    // get lcm of all the denominators of all the rational values in e
+    rational get_lcm(expr *e);
+
+    // converts all numerals and uninterpreted constants in fml to int
+    // assumes that fml is in sop
+    void to_int(expr_ref &fml);
 };
 
 class limit_num_generalizer : public lemma_generalizer {
