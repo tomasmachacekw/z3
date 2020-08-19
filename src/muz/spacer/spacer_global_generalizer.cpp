4bfcc8c6e1e6c273133bf5976f2217a6409e7253
Arie Gurfinkel <arie.gurfinkel@uwaterloo.ca>
2020-08-18 22:10:18 -0400

    chore(spacer): comment to review later

--- src/muz/spacer/spacer_global_generalizer.cpp
+++ src/muz/spacer/spacer_global_generalizer.cpp
@@ -284,6 +284,7 @@ static void to_real(const expr_ref_vector &fml, expr_ref &nw_fml) {
 
 /// Create new vars to compute convex cls
 void lemma_global_generalizer::add_dim_vars(const lemma_cluster &lc) {
+    // AG: review. This code looks fishy.
     const expr_ref &pattern = lc.get_pattern();
     expr_offset r;
     std::pair<unsigned, unsigned> v;
