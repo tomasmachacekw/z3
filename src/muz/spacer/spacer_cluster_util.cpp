eeb525d04829953a25d34fc9b711f7796d83e820
Arie Gurfinkel <arie.gurfinkel@uwaterloo.ca>
2020-09-15 18:51:25 -0400

    pass over spacer_cluster_util. Questions marked with AG

--- src/muz/spacer/spacer_cluster_util.cpp
+++ src/muz/spacer/spacer_cluster_util.cpp
@@ -3,11 +3,11 @@ Copyright (c) 2020 Arie Gurfinkel
 
 Module Name:
 
-    spacer_convex_closure.cpp
+    spacer_cluster_util.cpp
 
 Abstract:
 
-   Compute convex closure of polyhedra
+   Utility methods for clustering
 
 Author:
 
@@ -17,6 +17,7 @@ Author:
 Notes:
 
 --*/
+
 #include "ast/arith_decl_plugin.h"
 #include "ast/ast.h"
 #include "ast/ast_pp.h"
@@ -25,8 +26,9 @@ Notes:
 #include "ast/rewriter/rewriter_def.h"
 #include "ast/rewriter/th_rewriter.h"
 #include "muz/spacer/spacer_util.h"
+
 namespace {
-/// return true if \p f is an arithmetic <=, <, >, >=
+/// Returns true if \p f is an arithmetic <=, <, >, >=
 bool is_arith_ineq(expr *f, expr *&lhs, expr *&rhs, ast_manager &m) {
     arith_util m_arith(m);
     if (!(m_arith.is_arith_expr(f) && to_app(f)->get_num_args() == 2))
@@ -35,34 +37,35 @@ bool is_arith_ineq(expr *f, expr *&lhs, expr *&rhs, ast_manager &m) {
            m_arith.is_le(f, lhs, rhs) || m_arith.is_lt(f, lhs, rhs);
 }
 } // namespace
+
 namespace spacer {
-// 2 non-mul terms are compared according to their ids
-// non-mul term < mul term
-// if both are mul, the ids of second arguments are compared
+/// Arithmetic term order
 struct arith_add_less_proc {
     arith_util &m_arith;
 
     arith_add_less_proc(arith_util &arith) : m_arith(arith) {}
 
     bool operator()(expr *e1, expr *e2) const {
-        ast_lt_proc ast_lt;
+        if (e1 == e2) return false;
 
+        ast_lt_proc ast_lt;
         expr *k1 = nullptr, *t1 = nullptr, *k2 = nullptr, *t2 = nullptr;
+
+        // k1*t1 < k2*t2 iff  t1 < t2 or  t1 == t2 && k1 < k2
+        // k1 and k2 can be null
+
         if (!m_arith.is_mul(e1, k1, t1)) { t1 = e1; }
         if (!m_arith.is_mul(e2, k2, t2)) { t2 = e2; }
 
-        if (t1 == t2) {
-            // null < anything
-            if (!k1) return k1 != k2;
-            // k2 == null, k1 is not less than k2
-            else if (!k2)
-                return false;
-            // fall back
-            else
-                return ast_lt(k1, k2);
-        } else {
-            return ast_lt(t1, t2);
-        }
+        SASSERT(t1 && t2);
+        if (t1 != t2) return ast_lt(t1, t2);
+
+        //  here: t1 == t2 && k1 != k2
+        SASSERT(k1 != k2);
+
+        // check for null
+        if (!k1 || !k2) return !k1;
+        return ast_lt(k1, k2);
     }
 };
 
@@ -72,27 +75,17 @@ struct bool_and_less_proc {
     bool_and_less_proc(ast_manager &mgr, arith_util &arith)
         : m(mgr), m_arith(arith) {}
 
-    // if e1 == e2, e1 < e2
-    // 2 non arithmetic terms are compared according to id
-    // non arithmetic term < arithmetic term
-    // negation is ignored when comparing 2 arithmetic terms
-    // compare lhs:
-    // if both are vars, compare id
-    // vars < non vars
-    // two apps are compared using their leading uninterpreted constant
-    // no uninterpreted constant < uninterpreted constant
-    // if they are the same, id of lhs is used
-    // a < not a
     bool operator()(expr *e1, expr *e2) const {
         expr *a1 = nullptr, *a2 = nullptr;
         bool is_not1, is_not2;
+        if (e1 == e2) return false;
+
         is_not1 = m.is_not(e1, a1);
         a1 = is_not1 ? a1 : e1;
         is_not2 = m.is_not(e2, a2);
         a2 = is_not2 ? a2 : e2;
 
-        if (a1 == a2) return is_not1 < is_not2;
-        return arith_lt(a1, a2);
+        return a1 == a2 ? is_not1 < is_not2 : arith_lt(a1, a2);
     }
 
     bool arith_lt(expr *e1, expr *e2) const {
@@ -112,17 +105,13 @@ struct bool_and_less_proc {
             k2 = nullptr;
         }
 
-        if (!k1 || !k2) {
-            if (k1 == k2) return ast_lt(t1, t2);
-            // either k1 or k2 are nullptr
-            return k1 < k2;
-        }
+        if (!k1 || !k2) { return k1 == k2 ? ast_lt(t1, t2) : k1 < k2; }
 
         if (t1 == t2) return ast_lt(k1, k2);
 
         if (!(is_app(t1) && is_app(t2))) {
-            if (is_app(t1) == is_app(t2)) return ast_lt(t1, t2);
-            return is_app(t1) < is_app(t2);
+            return is_app(t1) == is_app(t2) ? ast_lt(t1, t2)
+                                            : is_app(t1) < is_app(t2);
         }
 
         unsigned d1 = to_app(t1)->get_depth();
@@ -134,24 +123,19 @@ struct bool_and_less_proc {
 
         u1 = get_first_uc(t1);
         u2 = get_first_uc(t2);
-        if (!u1 || !u2) {
-            if (u1 == u2) return ast_lt(t1, t2);
-            return u1 < u2;
-        }
-
+        if (!u1 || !u2) { return u1 == u2 ? ast_lt(t1, t2) : u1 < u2; }
         return u1 == u2 ? ast_lt(t1, t2) : ast_lt(u1, u2);
     }
 
-    /// intends to extract the first uninterpreted constant of an arithmetic
-    /// expression return nullptr when no constant is found assumes input
-    /// expression e is shallow and uses recursion depth of recursion is the
-    /// depth of leftmost branch of ast
+    /// Returns first in left-most traversal uninterpreted constant of \p e
+    ///
+    /// Returns null when no uninterpreted constant is found.
+    /// Recursive, assumes that expression is shallow and recursion is bounded.
     expr *get_first_uc(expr *e) const {
         expr *t, *k;
         if (is_uninterp_const(e))
             return e;
         else if (m_arith.is_add(e)) {
-            /// HG: never going to happen ?
             if (to_app(e)->get_num_args() == 0) return nullptr;
             expr *a1 = to_app(e)->get_arg(0);
             // HG: for 3 + a, returns nullptr
@@ -162,7 +146,8 @@ struct bool_and_less_proc {
         return nullptr;
     }
 };
-// Rewriting arithmetic expressions based on term order
+
+// Rewriter for normalize_order()
 struct term_ordered_rpp : public default_rewriter_cfg {
     ast_manager &m;
     arith_util m_arith;
@@ -181,73 +166,87 @@ struct term_ordered_rpp : public default_rewriter_cfg {
         br_status st = BR_FAILED;
 
         if (is_add(f)) {
-            ptr_buffer<expr> v;
-            v.append(num, args);
-            std::stable_sort(v.c_ptr(), v.c_ptr() + v.size(), m_add_less);
-            result = m_arith.mk_add(num, v.c_ptr());
+            ptr_buffer<expr> kids;
+            kids.append(num, args);
+            std::stable_sort(kids.c_ptr(), kids.c_ptr() + kids.size(),
+                             m_add_less);
+            result = m_arith.mk_add(num, kids.c_ptr());
             return BR_DONE;
         }
 
         if (m.is_and(f)) {
-            ptr_buffer<expr> v;
-            v.append(num, args);
-            std::stable_sort(v.c_ptr(), v.c_ptr() + v.size(), m_and_less);
-            result = m.mk_and(num, v.c_ptr());
+            ptr_buffer<expr> kids;
+            kids.append(num, args);
+            std::stable_sort(kids.c_ptr(), kids.c_ptr() + kids.size(),
+                             m_and_less);
+            result = m.mk_and(num, kids.c_ptr());
             return BR_DONE;
         }
         return st;
     }
 };
 
+// Normalize an arithmetic expression using term order
 void normalize_order(expr *e, expr_ref &out) {
     params_ref params;
-    // arith_rewriter
+    // -- arith_rewriter params
     params.set_bool("sort_sums", true);
     // params.set_bool("gcd_rounding", true);
     // params.set_bool("arith_lhs", true);
-    // poly_rewriter
+    // -- poly_rewriter params
     // params.set_bool("som", true);
     // params.set_bool("flat", true);
 
-    // apply rewriter
+    // apply theory rewriter
     th_rewriter rw(out.m(), params);
     rw(e, out);
 
     STRACE("spacer_normalize_order'",
            tout << "OUT Before:" << mk_pp(out, out.m()) << "\n";);
+    // apply term ordering
     term_ordered_rpp t_ordered(out.m());
     rewriter_tpl<term_ordered_rpp> t_ordered_rw(out.m(), false, t_ordered);
     t_ordered_rw(out.get(), out);
     STRACE("spacer_normalize_order'",
            tout << "OUT After :" << mk_pp(out, out.m()) << "\n";);
 }
-/// given an arithmetic inequality \p lit, try to compute \p t and \p c such
-/// that (t <= c) ==> lit and c is a numeral returns true if such a t, c can be
-/// found. false otherwise
+
+/// Under approximate arithmetic inequality
+///
+/// Returns true if there are \p t and \p c such that (t <= c) ==> lit
 bool under_approx_using_le(expr *lit, expr_ref &t, expr_ref &c) {
+    // AG: I do not see where the *under-approximation* happens
+    // AG: Seems more like is_le(lit, t, c)
+    // AG: Maybe I missed something in refactoring
+    ast_manager &m = t.get_manager();
+    arith_util m_arith(m);
+
     expr *e0 = nullptr, *e1 = nullptr, *e2 = nullptr;
     rational n;
     bool is_int = true;
-    ast_manager &m = t.get_manager();
-    arith_util m_arith(m);
     bool is_not = m.is_not(lit, e0);
     if (!is_not) e0 = lit;
-    //if lit is not an arithmetic inequality, it is not handled
+
     if (!is_arith_ineq(e0, e1, e2, m)) return false;
+
     if (!m_arith.is_numeral(e2, n, is_int)) return false;
+
     t = e1;
-    if ((!is_not && m_arith.is_le(e0)) || (is_not && m_arith.is_gt(e0))) c = e2;
-    if ((!is_not && m_arith.is_lt(e0)) || (is_not && m_arith.is_ge(e0))) {
-        // x <= (k-1) ==> x < k
+    if ((!is_not && m_arith.is_le(e0)) || (is_not && m_arith.is_gt(e0)))
+        c = e2;
+    else if ((!is_not && m_arith.is_lt(e0)) || (is_not && m_arith.is_ge(e0))) {
+        if (!is_int) return false;
+        // t < n ==> t <= (n-1)
         c = m_arith.mk_numeral(n - 1, is_int);
-    }
-    if ((!is_not && m_arith.is_gt(e0)) || (is_not && m_arith.is_le(e0))) {
-        // -x <= -k - 1 ==> x > k
+    } else if ((!is_not && m_arith.is_gt(e0)) ||
+               (is_not && m_arith.is_le(e0))) {
+        if (!is_int) return false;
+        // t > n ==> -t <= -n - 1
         mul_by_rat(t, rational::minus_one());
         c = m_arith.mk_numeral(-n - 1, is_int);
-    }
-    if ((!is_not && m_arith.is_ge(e0)) || (is_not && m_arith.is_lt(e0))) {
-        // -x <= -k ==> x >= k
+    } else if ((!is_not && m_arith.is_ge(e0)) ||
+               (is_not && m_arith.is_lt(e0))) {
+        // t >= n ==> -t <= -n
         mul_by_rat(t, rational::minus_one());
         c = m_arith.mk_numeral(-n, is_int);
     }
@@ -255,11 +254,13 @@ bool under_approx_using_le(expr *lit, expr_ref &t, expr_ref &c) {
     return true;
 }
 
-/// multiply \p fml with \p num and simplify
+/// Multiply an expression \p fml by a rational \p num
 ///
-/// fml should be of sort in Int, Real, BV
+/// \p fml should be of sort Int, Real, or BitVec
+/// multiplication is simplifying
 void mul_by_rat(expr_ref &fml, rational num) {
     if (num.is_one()) return;
+
     ast_manager &m = fml.get_manager();
     arith_util m_arith(m);
     bv_util m_bv(m);
@@ -267,22 +268,22 @@ void mul_by_rat(expr_ref &fml, rational num) {
     SASSERT(m_arith.is_int_real(fml) || m_bv.is_bv(fml));
     if (m_arith.is_int_real(fml)) {
         e = m_arith.mk_mul(m_arith.mk_numeral(num, m_arith.is_int(fml)), fml);
-    }
-    else if (m_bv.is_bv(fml)) {
+    } else if (m_bv.is_bv(fml)) {
         unsigned sz = m_bv.get_bv_size(fml);
         e = m_bv.mk_bv_mul(m_bv.mk_numeral(num, sz), e);
     }
+
+    // use theory rewriter to simplify
     params_ref params;
-    // poly_rewriter
     params.set_bool("som", true);
     params.set_bool("flat", true);
-
-    // apply rewriter
     th_rewriter rw(fml.m(), params);
     rw(e, fml);
 }
 
 namespace extract_nums_ns {
+
+/// Extract numerals from an expression
 struct extract_nums_proc {
     ast_manager &m;
     arith_util m_arith;
@@ -292,18 +293,16 @@ struct extract_nums_proc {
     void operator()(expr *n) const {}
     void operator()(app *n) {
         rational val;
-        if (m_arith.is_numeral(n, val) && !m_res.contains(val))
-            m_res.push_back(val);
+        if (m_arith.is_numeral(n, val)) m_res.push_back(val);
     }
 };
 } // namespace extract_nums_ns
 
-// get all numeral values in the formula
-// run time is number of nodes in e * res.size !!!
-// TODO: use sets to speed things up ?
+/// Extract all numerals from an expression
 void extract_nums(expr_ref e, vector<rational> &res) {
-    extract_nums_ns::extract_nums_proc t(e.get_manager(), res);
-    for_each_expr(t, e);
+    extract_nums_ns::extract_nums_proc proc(e.get_manager(), res);
+    // assuming for_each_expr is caching, this is linear in the dag size of e
+    for_each_expr(proc, e);
 }
 } // namespace spacer
 template class rewriter_tpl<spacer::term_ordered_rpp>;
